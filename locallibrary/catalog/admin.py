61092c3ceb32eabc7ea41d583d14c5e0d21cb5b2
jer <mjlerba@gmail.com>
2026-01-20 12:46:40 +0200

    updating catalog/admin.py and catalog/models.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+#admin.site.register(Author)
+class AuthorInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [AuthorInline]
+    fields = ('first_name', 'last_name', ('date_of_birth', 'date_of_death'))
+                
+            
+            
+            
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
+    
+    fieldsets = (
+            ('Informations', {
+                'fields': ('book','imprint', 'id'),
+                
+                }),
+            ('Availability', {
+                'fields': ('status', 'due_back'),
+                }
+            ))
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+admin.site.register(Language)
